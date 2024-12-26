290abf70236883fb5ddce50e6900af02f17fe9fd
ncyerage23 <ncyerage@gmail.com>
2024-12-26 14:28:14 -0500

    restarting

--- mainloop.py
+++ mainloop.py
@@ -1,39 +1,24 @@
 '''
-Mainloop for Eerie PA display -- 12/26/24
+Mainloop for Eerie PA game
 '''
 
 from setup import *
-import entity as en
-import display as dis
 
 
-screen = pygame.display.set_mode((1000, 750))
+screen = pygame.display.set_mode(WIN_SIZE)
 pygame.display.set_caption('Eerie, PA')
 clock = pygame.time.Clock()
 
-screen.fill( (0,0,0) )
-
-p1 = en.Player()
-d = dis.Display()
-test = en.Test((200, 200))
+screen.fill( BLACK )
 
 
 running = True
 while running:
-
-    key = None
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
     
-    p1.update(pygame.key.get_pressed())
-    d.update(p1.coords)
-    test.update(p1.coords)
 
-    screen.fill( (0,0,0) )
-    d.draw(screen)
-    p1.draw(screen)
-    test.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 '''
-Setup file for Eerie, PA
+File for constants and stuff for Eerie PA
 '''
-
 
 import pygame
 from pygame.locals import (
@@ -20,16 +19,16 @@ from pygame.locals import (
     K_d
 )
 
-import random
-import math
-
 pygame.display.init()
 pygame.font.init()
 
-MOVEMENT_KEYS = [K_w, K_a, K_s, K_d]
-MOVEMENTS = {
-    K_d: (1, 0),
-    K_a: (-1, 0),
-    K_s: (0, -1), 
-    K_w: (0, 1)
-}
+
+#Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+#Window Stuff
+WIN_SIZE = (1000, 750)
