3a23f7e315bce20840e37c22ed0c4e2d1efc9851
ncyerage23 <ncyerage@gmail.com>
2024-12-26 16:53:34 -0500

    not sure rn

---
+++ test.py
@@ -0,0 +1,21 @@
+'''
+Main file for running Eerie PA (2d prototype version)
+'''
+
+
+"""
+So, here's what I'm going to be doing. I'm going to plan out how it'll all work,
+then I'll start to build it piece by piece, you know? Keep it as simple as possible.
+
+
+I'm not even sure how it's going to work, from the lowest level. Idek.
+
+
+So, there's the outside world, and then different buildings you can enter.
+The buildings are separate maps, and you can move around in them.
+To enter the buildings, you walk up to the door and enter, and it removes the outside world
+and plops you in that building.
+
+
+Essentially
+"""
